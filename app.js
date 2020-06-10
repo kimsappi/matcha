7104503f7d06c1ef7c0e3bec1517f5bec2db847d
Kim Säppi <sappikim@gmail.com>
2020-06-10 21:04:40 +0300

    Early work on DB and sessions

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const config = require('./config.json');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: config.secret}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
