80f00ab7d4cdc6c796d70353f7321b95c4235b94
Kim Sappi <ksappi@student.hive.fi>
2020-07-12 13:33:19 +0300

    Profile visits are now logged

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,11 @@ const mysql = require('mysql');
 
 const pool = require('../modules/dbConnect');
 const {getGenderEmoji} = require('../modules/gender.js');
+const mysqlDatetime = require('../modules/mysqlDatetime');
+
+const getLikeButtonStatus = (user, other) => {
+	
+};
 
 const get = (req, res, next) => {
 	const userId = req.params.id;
@@ -18,13 +23,23 @@ const get = (req, res, next) => {
 		if (error || !results || !results[0])
 			return res.status(301).redirect('/');
 		
+		const visitQuery = `DELETE FROM visits WHERE visitor = ? AND visitee = ?;
+		INSERT INTO visits (visitor, visitee, \`time\`) VALUES (?, ?, ?);`
+		const preparedVisitQuery = mysql.format(visitQuery, [
+			req.session.user.id, results[0].id, req.session.user.id, results[0].id, mysqlDatetime(new Date())
+		]);
+
+		// Don't really care about success so no callback
+		pool.query(preparedVisitQuery);
+
 		return res.render('profile', 
 			{
 				user: req.session.user,
 				profileData: results[0],
 				lookingFor: getGenderEmoji(results[0].target_genders),
 				gender: getGenderEmoji(results[0].gender),
-				title: `${results[0].first_name} ${results[0].last_name[0]}.`
+				title: `${results[0].first_name} ${results[0].last_name[0]}.`,
+				likeButton: getLikeButtonStatus(req.session.user, results[0])
 			}
 		);
 	});
